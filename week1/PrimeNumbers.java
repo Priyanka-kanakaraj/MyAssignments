64a4317a867e87a399c45987086bd74cf17d41a9
Priyanka-kanakaraj <kpriya24599@gmail.com>
2024-11-13 20:15:38 +0530

    PrimeNumbers.java

--- week1/PrimeNumbers.java
+++ week1/PrimeNumbers.java
@@ -3,35 +3,31 @@ package HomeAssignment.week1;
 import java.util.Scanner;
 
 public class PrimeNumbers {  
-    public static void main(String[] args) {
-    	int num,i,count=0;
-    	System.out.println("Enter the Number:");
-    	Scanner sc= new Scanner(System.in);
-    	num= sc.nextInt();
-    	if(num==0||num==1)
-    	{
-    		System.out.println(num+ " is not a prime Number");
-    }
-    	{
-    	for (i=1;i<=num;i++)
-    		{
-    			if(num%i==0)
-    			{
-    				count++;
-    			}
-    		}
-    	if(count ==2)
-    	{
-    		System.out.println(num+" is a prime Number");
-    	}
-    		else
-    		{
-    			System.out.println(num+ " is not a prime Number");
-    		
-    	}
-    }
-    	}
-    	
-}
+	    public static void main(String[] args) 
+	    {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int n = sc.nextInt();
+	        if (isPrime(n)) {
+	            System.out.println(n + " is a prime number.");
+	        } else {
+	            System.out.println(n + " is a not a prime number.");
+	        }
+	        sc.close();
+	    }
+	    public static boolean isPrime(int n)
+	    {
+	        if (n <= 1) {
+	            return false;
+	        }
+	        for (int i = 2; i < n; i++) {
+	            if (n % i == 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+	}
+
 
 
